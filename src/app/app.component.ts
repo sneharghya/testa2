d8222199aa8969ef54addb0030b26909f358787c
sneharghya <snp.scorpio@gmail.com>
2017-07-29 21:46:33 +0530

    initall test commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { IMyDpOptions } from "mydatepicker";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  dateForm: FormGroup;
+
+  private myDatePickerOptions: IMyDpOptions = {
+        // other options...
+        dateFormat: 'mm/dd/yyyy',
+    };
+
+  constructor( private _fb: FormBuilder ){}
+
+  ngOnInit(): void {
+    this.dateForm = this._fb.group({
+      myDate: [ null , [ Validators.required ] ]
+    })
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms'; 
+
+import { MyDatePickerModule } from 'mydatepicker';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +11,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, ReactiveFormsModule, MyDatePickerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
